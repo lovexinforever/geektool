198ce82dbec19ae4e1de82c9c766ce3dada71590
yang.ding <yang.ding@99bill.com>
2019-01-08 10:56:04 +0800

    update icon dist

--- icon.py
+++ icon.py
@@ -12,5 +12,4 @@ def queryIcon(key):
     # 如果获取不到,直接给默认
     else:
         icon = "https://raw.githubusercontent.com/lovexinforever/blog_back_up/master/blog_photos/20101027235020136.png"
-    print(icon)
     return icon;
